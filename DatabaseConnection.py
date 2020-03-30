95529846e0914af2a9581358e48c1fb805d3d77c
sagardas1 <37270042+sagardas1@users.noreply.github.com>
2020-03-31 01:08:08 +0530

    db_connection

---
+++ DatabaseConnection.py
@@ -0,0 +1,13 @@
+import mysql.connector
+"""connect to db"""
+mydb= mysql.connector.connect(host="localhost" ,user  ="root",password="Sagardas@12345",database="pythonDb")
+
+"""cursor is like pointers,its points to a particular location"""
+myCursor=mydb.cursor()
+"""here to write sql quries"""
+myCursor.execute('insert into student(name,schoolName) values("rrr","dav public school")')
+"""fetching data from db and storing in list that all myCursor.fetchall() do"""
+result=myCursor.fetchall()
+
+
+mydb.commit()
